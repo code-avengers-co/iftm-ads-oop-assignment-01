0bf81e35adcf973791029d4b974ed63ce99a4860
Dev Luquinha <lucasemmanuelrodrigues737@gmail.com>
2026-04-29 00:27:56 -0300

    FEAT: Create the Product java class

---
+++ src/model/Product.java
@@ -0,0 +1,67 @@
+package model;
+
+import java.time.LocalDateTime;
+
+public class Product {
+    private int id;
+    private String name;
+    private String description;
+    private double price;
+    private boolean isActive;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public Product(int id, String name, double price){
+        this.name = name;
+        this.price = price;
+        this.createdAt = LocalDateTime.now();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public boolean getIsActive() {
+        return isActive;
+    }
+
+    public void setIsActive(boolean isActive) {
+        this.isActive = isActive;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
