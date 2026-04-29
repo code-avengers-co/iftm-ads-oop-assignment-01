7cf9d6600c6613d9732e5c33510383d8bfe9d737
Dev Luquinha <lucasemmanuelrodrigues737@gmail.com>
2026-04-29 19:14:33 -0300

    FEAT: Cleanup ProductController class

--- src/controller/ProductController.java
+++ src/controller/ProductController.java
@@ -1,52 +1,20 @@
 package controller;
 
 import dao.ProductDao;
-import java.util.Scanner;
+import model.Product;
 
 public class ProductController {
     private ProductDao productDao;
-    private Scanner scanner;
 
     public ProductController(ProductDao productDao){
         this.productDao = productDao;
-        this.scanner = new Scanner(System.in);
     }
 
-    public void showMenu(){
-        int option;
-        do {
-            option = getOption();
-            switch (option){
-                case 1:
-                    System.out.println("Creating a new product...");
-                    break;
-                case 2:
-                    System.out.println("Listing all products...");
-                    break;
-                case 0:
-                    System.out.println("Exiting...");
-                    break;
-            }
+    public boolean registerProduct(int id, String name, double price) {
+        // 1. Create the product
+        Product newProduct = new Product(id, name, price);
 
-        } while (option != 0);
-    }
-
-    private int getOption(){
-        String menu = """
-                --- PRODUCT MENU ---
-                1 - Register Product
-                2 - List Products
-                0 - Back
-                """;
-        System.out.print(menu);
-
-        int option = -1;
-        do{
-            System.out.print("Choose an option: ");
-            option = Integer.parseInt(scanner.nextLine());
-
-        } while (option < 0 || option > 2);
-
-        return option;
+        // 2. Send to DAO
+        return productDao.saveProduct(newProduct);
     }
 }
