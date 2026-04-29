68eec72cd433ab271e281b6277e77b287fec4dd1
Dev Luquinha <lucasemmanuelrodrigues737@gmail.com>
2026-04-29 18:39:46 -0300

    FEAT: Create the menu in ProductController

--- src/controller/ProductController.java
+++ src/controller/ProductController.java
@@ -13,6 +13,40 @@ public class ProductController {
     }
 
     public void showMenu(){
+        int option;
+        do {
+            option = getOption();
+            switch (option){
+                case 1:
+                    System.out.println("Creating a new product...");
+                    break;
+                case 2:
+                    System.out.println("Listing all products...");
+                    break;
+                case 0:
+                    System.out.println("Exiting...");
+                    break;
+            }
 
+        } while (option != 0);
+    }
+
+    private int getOption(){
+        String menu = """
+                --- PRODUCT MENU ---
+                1 - Register Product
+                2 - List Products
+                0 - Back
+                """;
+        System.out.print(menu);
+
+        int option = -1;
+        do{
+            System.out.print("Choose an option: ");
+            option = Integer.parseInt(scanner.nextLine());
+
+        } while (option < 0 || option > 2);
+
+        return option;
     }
 }
