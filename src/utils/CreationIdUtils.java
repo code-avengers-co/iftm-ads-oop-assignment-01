80216ea8b7d544bffa70d9bfae8c040ead1a1520
Dev Luquinha <lucasemmanuelrodrigues737@gmail.com>
2026-05-01 12:22:27 -0300

    FEAT: Add nextPersonId and nextUserId in utils class

--- src/utils/CreationIdUtils.java
+++ src/utils/CreationIdUtils.java
@@ -2,10 +2,24 @@ package utils;
 
 public class CreationIdUtils {
     private static int nextProductId = 1;
+    private static int nextPersonId = 1;
+    private static int nextUserId = 1;
 
     public static int getNextProductId(){
         int idToReturn = nextProductId;
         nextProductId++;
+        return idToReturn;
+    }
+
+    public static int getNextPersonId(){
+        int idToReturn = nextPersonId;
+        nextPersonId++;
+        return idToReturn;
+    }
+
+    public static int getNextUserId(){
+        int idToReturn = nextUserId;
+        nextUserId++;
         return idToReturn;
     }
 }
