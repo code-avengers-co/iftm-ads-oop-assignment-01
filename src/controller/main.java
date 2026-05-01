b6062657ae77d42e0283ebb5a0549b8be264e739
Dev Luquinha <lucasemmanuelrodrigues737@gmail.com>
2026-05-01 10:56:15 -0300

    Release 1.0.0
    Just the Product CRUD working

--- src/controller/main.java
+++ src/controller/main.java
@@ -7,16 +7,10 @@ import view.ProductView;
 
 public class main {
     static void main(String[] args) {
-        //ProductDao dao = new ProductDao(100);
-        //ProductController controller = new ProductController(dao);
-        //ProductView view = new ProductView(controller);
+        ProductDao dao = new ProductDao(100);
+        ProductController controller = new ProductController(dao);
+        ProductView view = new ProductView(controller);
 
-        //view.showMenu();
-
-        CouponDao couponDao = new CouponDao(100);
-        CouponController couponController = new CouponController(couponDao);
-        CouponView couponView = new CouponView(couponController);
-
-        couponView.showMenu();
+        view.showMenu();
     }
 }
