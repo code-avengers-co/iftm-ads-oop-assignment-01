951350f63a703ff11f81dbac3bae33d18714e46b
Dev Luquinha <lucasemmanuelrodrigues737@gmail.com>
2026-05-01 11:52:11 -0300

    FEAT: Create the getUsers method

--- src/dao/UserDao.java
+++ src/dao/UserDao.java
@@ -15,10 +15,24 @@ public class UserDao {
         if (userCount >= userDb.length) {
             return false; // No more space to save new user
         }
-        
+
         userDb[userCount] = user;
         userCount++;
 
         return true;
+    }
+
+    public User[] getUsers(){
+        User[] users = new User[userCount];
+        int currentIndex = 0;
+
+        for (int i = 0; i < userCount; i++) {
+            if (userDb[i] != null){
+                users[currentIndex]= userDb[i];
+                currentIndex++;
+            }
+        }
+
+        return users;
     }
 }
