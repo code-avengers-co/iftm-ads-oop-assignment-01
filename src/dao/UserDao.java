b39332547814c812afce5f1a6b0bba325c44b9f7
Dev Luquinha <lucasemmanuelrodrigues737@gmail.com>
2026-05-01 11:58:56 -0300

    FEAT: Implement updateUser and deleteUser methods in UserDao

--- src/dao/UserDao.java
+++ src/dao/UserDao.java
@@ -2,6 +2,8 @@ package dao;
 
 import model.User;
 
+import java.time.LocalDateTime;
+
 public class UserDao {
     private User[] userDb;
     private int userCount;
@@ -46,5 +48,38 @@ public class UserDao {
         return null; // User not found
     }
 
-    
+    public boolean updateUser(User updatedUser) {
+        User user = findById(updatedUser.getId());
+        if (user == null) {
+            return false; // User not found
+        }
+
+        user.setPerson(updatedUser.getPerson());
+        user.setUsername(updatedUser.getUsername());
+        user.setPassword(updatedUser.getPassword());
+        user.setUpdatedAt(LocalDateTime.now());
+
+        return true;
+    }
+
+    public boolean deleteUser(int id) {
+        int indexToDeleted = -1;
+        for (int i = 0; i < userCount; i++) {
+            if (this.userDb[i].getId() == id){
+                indexToDeleted = i;
+                break;
+            }
+        }
+
+        if (indexToDeleted == -1){
+            return false; // User not found
+        }
+
+        // Shift the last user to index to be deleted and nullify the last position
+        userDb[indexToDeleted] = userDb[userCount - 1];
+        userDb[userCount - 1] = null;
+        userCount--;
+
+        return true;
+    }
 }
