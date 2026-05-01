b7fb81246cde7fff012567dbd69006914d6811fc
Dev Luquinha <lucasemmanuelrodrigues737@gmail.com>
2026-05-01 12:46:20 -0300

    FEAT: Update the main class

--- src/main.java
+++ src/main.java
@@ -1,16 +1,30 @@
 import controller.CouponController;
 import controller.ProductController;
+import controller.UserController;
 import dao.CouponDao;
+import dao.PersonDao;
 import dao.ProductDao;
+import dao.UserDao;
 import view.CouponView;
 import view.ProductView;
+import view.UserView;
 
 public class main {
     static void main(String[] args) {
-        ProductDao dao = new ProductDao(100);
-        ProductController controller = new ProductController(dao);
-        ProductView view = new ProductView(controller);
+        // 1. Create DAO (Simulate DATABASE)
+        ProductDao productDao = new ProductDao(100);
+        PersonDao personDao = new PersonDao(100);
+        UserDao userDao = new UserDao(100);
 
-        view.showMenu();
+        // 2. Create Controllers
+        ProductController productController = new ProductController(productDao);
+        UserController userController = new UserController(userDao, personDao);
+
+        // 3. Create Views
+        ProductView productView = new ProductView(productController);
+        UserView userView = new UserView(userController);
+
+        // 4. Call test
+        userView.showMenu();
     }
 }
