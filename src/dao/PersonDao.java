dcdcd797fca94db10fc46ecdf6c532dae3659912
Dev Luquinha <lucasemmanuelrodrigues737@gmail.com>
2026-05-01 11:29:21 -0300

    FEAT: Create the savePerson and getAllPersons

--- src/dao/PersonDao.java
+++ src/dao/PersonDao.java
@@ -10,4 +10,28 @@ public class PersonDao {
         this.personDb = new Person[length];
         this.personCount = 0;
     }
+
+    public boolean savePerson(Person person) {
+        if (personCount >= personDb.length) {
+            return false; // No more space to save new person
+        }
+        personDb[personCount] = person;
+        personCount++;
+
+        return true;
+    }
+
+    public Person[] getAllPersons() {
+        Person[] allPersons = new Person[personCount];
+        int currentIndex = 0;
+
+        for (int i = 0; i < personCount; i++) {
+            if (personDb[i] != null) {
+                allPersons[currentIndex] = personDb[i];
+                currentIndex++;
+            }
+        }
+
+        return allPersons;
+    }
 }
