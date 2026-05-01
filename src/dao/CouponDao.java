feb8d31de10f91f5dd03b23eea5d8e71db7e3f0e
Jose Macciotti <jmacciottineto@gmail.com>
2026-05-01 09:51:08 -0300

    FEAT: Add basic DAO class to coupon

---
+++ src/dao/CouponDao.java
@@ -0,0 +1,14 @@
+package dao;
+
+import model.Coupon;
+import model.Product;
+
+import java.time.LocalDateTime;
+
+public class CouponDao {
+    private Coupon[] coupons;
+    private int count;
+
+    public CouponDao() {
+    }
+}
