0e2d176b1c20f485f6e8fecb637084622d72a34f
Dev Luquinha <lucasemmanuelrodrigues737@gmail.com>
2026-05-02 11:46:00 -0300

    FEAT: Create the Cart class

---
+++ src/model/Cart.java
@@ -0,0 +1,53 @@
+package model;
+
+import model.enums.CartStatus;
+
+import java.time.LocalDateTime;
+
+public class Cart {
+    private int id;
+    private User user;
+    private CartStatus status;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public Cart(int id, User user, CartStatus status) {
+        this.id = id;
+        this.user = user;
+        this.status = status;
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public CartStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(CartStatus status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
