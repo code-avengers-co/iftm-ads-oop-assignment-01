885c4e0a1337fb2ffefad75afddb6ea86cfa3936
Dev Luquinha <lucasemmanuelrodrigues737@gmail.com>
2026-05-02 17:31:13 -0300

    FEAT: Implement CheckoutController to handle checkout process

---
+++ src/controller/CheckoutController.java
@@ -0,0 +1,76 @@
+package controller;
+
+import dao.CartDao;
+import dao.CartItemDao;
+import dao.OrderDao;
+import dao.OrderItemDao;
+import model.*;
+import utils.CreationIdUtils;
+import utils.UserSession;
+
+public class CheckoutController {
+    private CartDao cartDao;
+    private CartItemDao cartItemDao;
+    private OrderDao orderDao;
+    private OrderItemDao orderItemDao;
+
+    public CheckoutController(CartDao cartDao, CartItemDao cartItemDao, OrderDao orderDao, OrderItemDao orderItemDao) {
+        this.cartDao = cartDao;
+        this.cartItemDao = cartItemDao;
+        this.orderDao = orderDao;
+        this.orderItemDao = orderItemDao;
+    }
+
+    public boolean processCheckout(String paymentMethod) {
+        // 1. Check user session
+        if (!UserSession.isLoggedIn()) {
+            return false;
+        }
+
+        User user = UserSession.getLoggedUser();
+
+        // 2. Get the Open Cart for the User
+        Cart cart = cartDao.findOpenCartByUser(user.getId());
+        if (cart == null) {
+            return false; // Not cart open
+        }
+
+        CartItem[] cartItems = cartItemDao.findItemsByCartId(cart.getId());
+        if (cartItems.length == 0) {
+            return false; // Empty cart
+        }
+
+        // 3. Get the total value
+        double totalValue = 0;
+        for (CartItem item : cartItems) {
+            totalValue += item.getQuantity() * item.getUnitPrice();
+        }
+
+        // 4. Create the order
+        Order newOrder = new Order(CreationIdUtils.generateOrderId(), user, totalValue, paymentMethod);
+
+        boolean orderSaved = orderDao.saveOrder(newOrder);
+        if (!orderSaved) {
+            return false;
+        }
+
+        // 5. Copy CartItems to OrderItems
+        for (CartItem cItem : cartItems) {
+            OrderItem oItem = new OrderItem(
+                    CreationIdUtils.generateOrderItemId(),
+                    newOrder,
+                    cItem.getProduct(),
+                    cItem.getQuantity(),
+                    cItem.getUnitPrice()
+            );
+
+            orderItemDao.saveOrderItem(oItem);
+        }
+
+        // 6. Close cart
+        cart.setStatus(model.enums.CartStatus.Closed);
+        cartDao.updateCart(cart);
+
+        return true;
+    }
+}
