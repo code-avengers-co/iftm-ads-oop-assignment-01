d9267bc3c26b37e2917599e2f6a2bd8b1dacbaa0
Dev Luquinha <lucasemmanuelrodrigues737@gmail.com>
2026-05-02 16:09:35 -0300

    FEAT: Create the OrderDao class

---
+++ src/dao/OrderDao.java
@@ -0,0 +1,38 @@
+package dao;
+
+import model.Order;
+
+public class OrderDao {
+    private Order[] orderDb;
+    private int orderCount;
+
+    public OrderDao(int length){
+        orderDb = new Order[length];
+        orderCount = 0;
+    }
+
+    public boolean saveOrder(Order order){
+        if (orderCount >= orderDb.length) {
+            return false; // No more space to save new order
+        }
+
+        orderDb[orderCount] = order;
+        orderCount++;
+
+        return true;
+    }
+
+    public Order[] getOrders(){
+        Order[] orders = new Order[orderCount];
+        int currentIndex = 0;
+
+        for (int i = 0; i < orderCount; i++) {
+            if (orderDb[i] != null){
+                orders[currentIndex]= orderDb[i];
+                currentIndex++;
+            }
+        }
+
+        return orders;
+    }
+}
