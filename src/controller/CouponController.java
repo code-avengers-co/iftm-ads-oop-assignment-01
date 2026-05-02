3f3912b624c2ac3bb6670aa2a9c90cbb52ae752a
Jose Macciotti <jmacciottineto@gmail.com>
2026-05-02 17:17:47 -0300

    FEAT: Add fundamental methods do Dao and Controller

--- src/controller/CouponController.java
+++ src/controller/CouponController.java
@@ -2,6 +2,7 @@ package controller;
 
 import dao.CouponDao;
 import model.Coupon;
+import model.Product;
 
 import java.time.LocalDateTime;
 
@@ -15,4 +16,14 @@ public class CouponController {
 
         return couponDao.saveCoupon(newCoupon);
     }
+
+    public Coupon findByid(int id) { return couponDao.findById(id); }
+
+    public Coupon[] getValidCoupons() {
+        return couponDao.getValidCoupons();
+    }
+
+    public Coupon[] getCoupons() { return couponDao.getCoupons(); }
+
+    public boolean deleteCoupon(String code) { return couponDao.deleteCoupon(code); }
 }
