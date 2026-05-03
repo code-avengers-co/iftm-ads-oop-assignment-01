ab2487836e529c30892c257c7f6c74406f59251b
Dev Luquinha <lucasemmanuelrodrigues737@gmail.com>
2026-05-03 11:23:56 -0300

    FEAT: Implement stock validation and update during checkout process

--- src/controller/CheckoutController.java
+++ src/controller/CheckoutController.java
@@ -47,6 +47,12 @@ public class CheckoutController {
             return false; // Empty cart
         }
 
+        for (CartItem item : cartItems){
+            if (item.getQuantity() > item.getProduct().getStockQuantity()){
+                return false; // Not enough stock for product
+            }
+        }
+
         // 3. Get the total value
         double totalValue = 0;
         for (CartItem item : cartItems) {
@@ -62,16 +68,32 @@ public class CheckoutController {
         }
 
         // 5. Copy CartItems to OrderItems
-        for (CartItem cItem : cartItems) {
-            OrderItem oItem = new OrderItem(
+        for (CartItem cartItem : cartItems) {
+            // 5.1 Create OrderItem and Save it
+            OrderItem orderItem = new OrderItem(
                     CreationIdUtils.generateOrderItemId(),
                     newOrder,
-                    cItem.getProduct(),
-                    cItem.getQuantity(),
-                    cItem.getUnitPrice()
+                    cartItem.getProduct(),
+                    cartItem.getQuantity(),
+                    cartItem.getUnitPrice()
             );
+            orderItemDao.saveOrderItem(orderItem);
 
-            orderItemDao.saveOrderItem(oItem);
+            // 5.2 Create StockMovement and Save it
+            StockMovement movement = new StockMovement(
+                    CreationIdUtils.generateStockMovementId(),
+                    cartItem.getProduct(),
+                    cartItem.getQuantity(),
+                    model.enums.MovementType.OUT,
+                    cartItem.getUnitPrice()
+            );
+            stockMovementDao.saveStockMovement(movement);
+
+            // 5.3 Update Product Stock
+            Product product = cartItem.getProduct();
+            int newStock = product.getStockQuantity() - cartItem.getQuantity();
+            product.setStockQuantity(newStock);
+            productDao.updateProduct(product);
         }
 
         // 6. Close cart
